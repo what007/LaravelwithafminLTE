f995d037f484577c15bd53cfba694ca6faa8abb7
Satishwaran Raja Lingam - UEMS <satish.rajalingam@uemsunrise.com>
2022-11-24 12:24:29 +0800

    4th commit

--- resources/views/backend/leave/indexLeave.blade.php
+++ resources/views/backend/leave/indexLeave.blade.php
@@ -144,68 +144,68 @@
 
 @extends('backend.userLayouts.Umain')
 @section('content')
+    <div class="card-body" style="padding-top: 50px">
 
-<div class="card-body" style="padding-top: 50px">
+        <a href="{{ route('leave.create') }}">
+            <button type="button" class="btn btn-block btn-success" style="background: lightgreen"><b>Add<b></button>
+        </a>
 
-    <a href="{{route('leave.create')}}">
-        <button type="button" class="btn btn-block btn-success" style="background: lightgreen"><b>Add<b></button>
-    </a>
+        <p class="mb-10"></p>
 
-    <p class="mb-10"></p>
-
-    <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4">
-        <div class="row">
-            <div class="col-sm-12 col-md-6"></div>
-            <div class="col-sm-12 col-md-6"></div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <table id="example2" class="table table-bordered table-hover dataTable" role="grid"
-                    aria-describedby="example2_info">
-                    <thead> 
-                        {{-- 'name', 'email', 'leave_Type', 'start_Leave', 'end_Leave', 'leave_Reason' --}}
-                        <tr role="row" style="background: rgb(222, 188, 237)" >
-                            <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
-                                aria-label="Rendering engine: activate to sort column descending" aria-sort="ascending">
-                                Name</th>
-                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
-                                aria-label="Browser: activate to sort column ascending">E-mail</th>
-                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
-                                aria-label="Platform(s): activate to sort column ascending">Type of Leave</th>
-                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
-                                aria-label="Platform(s): activate to sort column ascending">Initial Date</th>
-                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
-                                aria-label="Engine version: activate to sort column ascending">End Date</th>
-                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
-                                aria-label="Engine version: activate to sort column ascending">Reason for leave</th>
-                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
-                                aria-label="CSS grade: activate to sort column ascending"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ( $data as $info )
-
-
-                        <tr role="row" class="odd" >
-                            <td class="sorting_1">{{$info->name}}</td>
-                            <td>{{$info->email}}</td>
-                            <td>{{$info->leave_type}}</td>
-                            <td>{{$info->start_leave}}</td>
-                            <td>{{$info->end_leave}}</td>
-                            <td>{{$info->leave_reason}}</td>
+        <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4">
+            <div class="row">
+                <div class="col-sm-12 col-md-6"></div>
+                <div class="col-sm-12 col-md-6"></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <table id="example2" class="table table-bordered table-hover dataTable" role="grid"
+                        aria-describedby="example2_info">
+                        <thead>
+                            {{-- 'name', 'email', 'leave_Type', 'start_Leave', 'end_Leave', 'leave_Reason' --}}
+                            <tr role="row" style="background: rgb(222, 188, 237)">
+                                <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1"
+                                    colspan="1" aria-label="Rendering engine: activate to sort column descending"
+                                    aria-sort="ascending">
+                                    Name</th>
+                                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                                    aria-label="Browser: activate to sort column ascending">E-mail</th>
+                                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                                    aria-label="Platform(s): activate to sort column ascending">Type of Leave</th>
+                                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                                    aria-label="Platform(s): activate to sort column ascending">Initial Date</th>
+                                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                                    aria-label="Engine version: activate to sort column ascending">End Date</th>
+                                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                                    aria-label="Engine version: activate to sort column ascending">Reason for leave</th>
+                                <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                                    aria-label="CSS grade: activate to sort column ascending"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $info)
+                                @if ('Controllers.LeaveController.store()' > 0)
+                                    
+                                    <tr role="row" class="odd">
+                                        <td class="sorting_1">{{ $info->name }}</td>
+                                        <td>{{ $info->email }}</td>
+                                        <td>{{ $info->leave_type }}</td>
+                                        <td>{{ $info->start_leave }}</td>
+                                        <td>{{ $info->end_leave }}</td>
+                                        <td>{{ $info->leave_reason }}</td>
 
 
-                            <td>
-                                <form action="{{route('leave.destroy', $info->id)}}" method="POST" >
-                                @csrf
-                                @method('DELETE')
-                                    <button class="btn btn-app" style="background: #ffcccb">
-                                        <i class="fas fa-delete" ></i> <b>Delete<b>
-                                    </button>
-                                </form>
-                            </td>
+                                        <td>
+                                            <form action="{{ route('leave.destroy', $info->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-app" style="background: #ffcccb">
+                                                    <i class="fas fa-delete"></i> <b>Delete<b>
+                                                </button>
+                                            </form>
+                                        </td>
 
-                            {{--
+                                        {{--
                         </tr>
                         <tr role="row" class="even">
                             <td class="sorting_1">Gecko</td>
@@ -264,36 +264,41 @@
                             <td>A</td>
                         </tr>
                         <tr role="row" class="even"> --}}
-                            {{-- <td class="sorting_1">Gecko</td>
+                                        {{-- <td class="sorting_1">Gecko</td>
                             <td>Mozilla 1.0</td>
                             <td>Win 95+ / OSX.1+</td>
                             <td>1</td>
                             <td>A</td> --}}
-                        </tr>
-                        @endforeach
+                                    </tr>
+                                @else
+                                    <td>{{ 'No data available in table' }}</td>
+                                    
+                                @endif
+                            @endforeach
 
 
-                    </tbody>
-                    <tfoot>
-                        {{-- <tr>
+                        </tbody>
+                        <tfoot>
+                            {{-- <tr>
                             <th rowspan="1" colspan="1">Rendering engine</th>
                             <th rowspan="1" colspan="1">Browser</th>
                             <th rowspan="1" colspan="1">Platform(s)</th>
                             <th rowspan="1" colspan="1">Engine version</th>
                             <th rowspan="1" colspan="1">CSS grade</th>
                         </tr> --}}
-                    </tfoot>
-                </table>
-                
-                {{$data->links()}}
+                        </tfoot>
+                    </table>
+
+
+                    {{ $data->links() }}
+                </div>
             </div>
-        </div>
-        <div class="row">
-            {{-- <div class="col-sm-12 col-md-5">
+            <div class="row">
+                {{-- <div class="col-sm-12 col-md-5">
                 <div class="dataTables_info" id="example2_info" role="status" aria-live="polite">Showing 1 to 10 of 57
                     entries</div>
             </div> --}}
-            {{-- <div class="col-sm-12 col-md-7">
+                {{-- <div class="col-sm-12 col-md-7">
                 <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
                     <ul class="pagination">
                         <li class="paginate_button page-item previous disabled" id="example2_previous"><a href="#"
@@ -316,8 +321,7 @@
                     </ul>
                 </div>
             </div> --}}
+            </div>
         </div>
     </div>
-</div>
-
 @endsection
